4247c639cff216bf3c65c32ce5d4e85257ce298c
kuber <kuberreddyp@gmail.com>
2024-10-31 14:44:36 +0530

    Rest end points were added in the citizen controller class

--- src/main/java/com/example/citizenservice/controller/CitizenController.java
+++ src/main/java/com/example/citizenservice/controller/CitizenController.java
@@ -1,8 +1,25 @@
 package com.example.citizenservice.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/citizen")
 public class CitizenController {
 
+	@PostMapping("/add")
+	public void addCitizen() {
+		
+	}
+	
+	
+	@PutMapping("/update")
+	public void updateCitizen() {
+		
+	}
+	
+	@GetMapping("/all")
+	public void getAllCitizens() {
+		
+	}
 }
