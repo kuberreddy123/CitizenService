30e63e0c6628ddda8ab977aa1c79ed4c2bb33a0b
kuber <kuberreddyp@gmail.com>
2024-10-31 13:12:23 +0530

    First commit

---
+++ src/main/java/com/example/citizenservice/CitiZenServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.citizenservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiZenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiZenServiceApplication.class, args);
+	}
+
+}
